1658b1864b9121750372c89d6b5373c838cbadb1
Atul Raj <atulsid151@gmail.com>
2020-08-12 00:16:57 +0530

    subscribe to the store

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 
-function App() {
-  return <div className="App">Hello World</div>;
+class App extends React.Component {
+  componentDidMount() {
+    //make api call
+    // dispatch action
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log("Updated");
+      this.forceUpdate();
+    });
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data,
+    });
+  }
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { createStore } from "redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Component/App";
+import movies from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const store = createStore(movies);
+// console.log("Before state", store.getState());
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "SuperMan" }],
+// });
+// console.log("After State", store.getState());
+
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
