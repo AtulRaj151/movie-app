a36049a6d10844c43b3c5a065337ef32ca892d94
Atul Raj <atulsid151@gmail.com>
2020-08-12 15:00:41 +0530

    added functionality to favourite and unfavourite movies

--- src/Component/App.js
+++ src/Component/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
-
+import { addMovies, setShowFavourites } from "../actions";
 class App extends React.Component {
   componentDidMount() {
     //make api call
@@ -12,26 +12,57 @@ class App extends React.Component {
       console.log("Updated");
       this.forceUpdate();
     });
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
+    console.log(this.props.store.getState());
   }
+  isFavouriteMovie = (movie) => {
+    const { favourite } = this.props.store.getState();
+    const index = favourite.indexOf(movie);
+
+    if (index != -1) {
+      return true;
+    }
+    return false;
+  };
+  changeTab = (val) => {
+    this.props.store.dispatch(setShowFavourites(val));
+  };
   render() {
-    const movies = this.props.store.getState();
+    const { list, favourite, showFavourites } = this.props.store.getState();
+    const displayMovies = showFavourites ? favourite : list;
+
+    console.log(showFavourites);
     return (
       <div className="App">
         <Navbar />
         <div className="main">
           <div className="tabs">
-            <div className="tab">Movies</div>
-            <div className="tab">Favourites</div>
+            <div
+              className={`tab ${showFavourites ? "" : "active-tabs"}`}
+              onClick={() => this.changeTab(false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavourites ? "active-tabs" : ""}`}
+              onClick={() => this.changeTab(true)}
+            >
+              Favourites
+            </div>
           </div>
           <div className="list">
-            {movies.map((movie, index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+            {displayMovies.map((movie, index) => (
+              <MovieCard
+                movie={movie}
+                key={`movies-${index}`}
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isFavouriteMovie(movie)}
+              />
             ))}
           </div>
+          {displayMovies.length == 0 ? (
+            <div className="no-movies">No movies to Display</div>
+          ) : null}
         </div>
       </div>
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,50 @@
-export default function movies(state = [], action) {
-  if (action.type === "ADD_MOVIES") {
-    return action.movies;
-  }
+import {
+  ADD_MOVIES,
+  ADD_TO_FAVOURITE,
+  REMOVE_FROM_FAVOURITE,
+  SET_SHOW_FAVOURITES,
+} from "../actions";
 
-  return state;
+const initialMovieState = {
+  list: [],
+  favourite: [],
+  showFavourites: false,
+};
+export default function movies(state = initialMovieState, action) {
+  // if (action.type === ADD_MOVIES) {
+  //   return {
+  //     ...state,
+  //     list: action.movies,
+  //   };
+  // }
+
+  // return state;
+
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+    case ADD_TO_FAVOURITE:
+      return {
+        ...state,
+        favourite: [action.movie, ...state.favourite],
+      };
+    case REMOVE_FROM_FAVOURITE:
+      const filteredArray = state.favourite.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favourite: filteredArray,
+      };
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val,
+      };
+    default:
+      return state;
+  }
 }
