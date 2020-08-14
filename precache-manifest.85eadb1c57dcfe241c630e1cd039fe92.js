8668725a0890db63a808bb8347572a9c7bfb6b4d
Atul Raj <atulsid151@gmail.com>
2020-08-14 10:47:33 +0530

    Updates

---
+++ precache-manifest.85eadb1c57dcfe241c630e1cd039fe92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771bc5bb874af7db0dbb745cd70761fd",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "8fb3a515aeddb6a0cbc1",
+    "url": "/movie-app/static/css/main.c3eac6f6.chunk.css"
+  },
+  {
+    "revision": "2d8fcfa539a2d8547f4e",
+    "url": "/movie-app/static/js/2.770788e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.770788e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb3a515aeddb6a0cbc1",
+    "url": "/movie-app/static/js/main.0b2d3744.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
